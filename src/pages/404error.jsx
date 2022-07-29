b082772d6b50c9b1b56583aa84611044ae36cb61
Thomas Nguyen <ttn@ou.edu>
2022-07-29 17:52:01 -0500

    Complete Tien Len page

--- src/pages/404error.jsx
+++ src/pages/404error.jsx
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom'
 const Error404 = () => {
     return (
         <div>
-            <h1>Page not found!</h1>
-            <Link to="/">Go back to the Spicy Cat home page!</Link>
-
+            <div className="wrapper">
+                <h1>Page not found!</h1>
+                <Link to="/">Go back to the Spicy Cat home page!</Link>
+            </div>
         </div>
     );
 };
--- src/pages/tienlen.jsx
+++ src/pages/tienlen.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import { React, useState } from "react";
 import Navbar from '../components/navbar';
+import './tienlen.css'
 
 const Tienlen = () => {
     return (
@@ -7,26 +8,59 @@ const Tienlen = () => {
             <Navbar />
 
             <div className="wrapper">
-                <h1>Hello a Edric</h1>
-                <p>Em thấy trang web a hay chơi Tiến Lên ko đẹp lắm cho nên em định tạo một trang web
-                    tiến lên cho mn chơi!
-                </p>
 
-                <Game />
+                <div className="tienlen">
+
+                    <Game />
+
+                    <p>Bạn có khi nào thua một ván Tiến Lên bởi vì bạn không nhớ những lá bài đã được chơi không?
+                        Hãy yên tâm, Spicy sẽ giúp bạn nhớ!</p>
+
+                </div>
             </div>
 
         </div>
     );
 };
 
-function Game() {
-    const cards = ['3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+function Counter(card) {
+    const [counter, setCounter] = useState(0);
+
+    const increase = () => {
+        if (counter < 4) {
+            setCounter(count => count + 1)
+        }
+    }
+
+    const decrease = () => {
+        if (counter > 0) {
+            setCounter(count => count - 1)
+        }
+    }
+
     return (
-        <ul>
+        <div className="cardButton">
+            <button className="minusBox" onClick={decrease}>{'-'}</button>
+            <p className="cardName"> {card} </p>
+            <button className="plusBox" onClick={increase}>{'+'}</button>
+            <p> {counter} </p>
+        </div>
+    )
+}
+
+
+function Game() {
+    const cards = ['3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', '2'];
+
+
+    return (
+        <div className="cardButtons">
             {cards.map((cards) => (
-                <li>{cards}</li>
+                <div>
+                    {Counter(cards)}
+                </div>
             ))}
-        </ul>
+        </div>
     );
 }
 
